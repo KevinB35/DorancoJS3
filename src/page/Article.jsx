e72bbfe969c79ccb62e068c58a7f3d87a594f20b
Kevin B <kevin.b@ikmail.com>
2023-08-10 10:49:48 +0200

    Exercice 6

--- src/page/Article.jsx
+++ src/page/Article.jsx
@@ -1,7 +1,7 @@
 import { Link, useNavigate, useParams } from "react-router-dom";
 import data from "../assets/data.json";
 import { useEffect, useState } from "react";
-import { Grid, Box, Typography, Table } from "@mui/material";
+import { Grid, Box, Typography, Alert } from "@mui/material";
 
 const Article = () => {
   const { id } = useParams();
@@ -21,49 +21,56 @@ const Article = () => {
   return (
     <>
       {article && (
-        <Grid container>
-          <Box width={"70%"} sx={{}}>
-            <Grid container textAlign={"center"} justifyContent={"center"}>
-              {Object.values(article.picture[0]).map((image, index) => (
-                <Link to={image} key={index} target='_blank'>
-                  <img
-                    src={image}
-                    alt={article.name}
-                    style={{ width: 400, height: 400, margin: 3 }}
-                  />
-                </Link>
-              ))}
-            </Grid>
-          </Box>
-          <Box
-            width={"30%"}
-            height={"100vh"}
-            sx={{
-              paddingLeft: 2,
-              textAlign: "center",
-              justifyContent: "center",
-              display: "flex",
-              flexDirection: "column",
-              alignItems: "center",
-            }}
-          >
-            <Typography variant='h3'>{article.name}</Typography>
-            <Typography variant='h4'>{article.price}€</Typography>
-            <Typography sx={{ margin: "20px 0" }}>{article.content}</Typography>
-            <Grid container>
-              {article.size.map((s, index) => (
-                <Grid
-                  item
-                  xs={6}
-                  sx={{ border: "1px solid lightgray", padding: 2 }}
-                  key={index}
-                >
-                  {s}
-                </Grid>
-              ))}
-            </Grid>
-          </Box>
-        </Grid>
+        <>
+          {(!article.online || article.stock === 0) && (
+            <Alert severity='error'>{"Cet article n'est pas en stock"}</Alert>
+          )}
+          <Grid container>
+            <Box width={"70%"} sx={{}}>
+              <Grid container textAlign={"center"} justifyContent={"center"}>
+                {Object.values(article.picture[0]).map((image, index) => (
+                  <Link to={image} key={index} target='_blank'>
+                    <img
+                      src={image}
+                      alt={article.name}
+                      style={{ width: 400, height: 400, margin: 3 }}
+                    />
+                  </Link>
+                ))}
+              </Grid>
+            </Box>
+            <Box
+              width={"30%"}
+              height={"100vh"}
+              sx={{
+                paddingLeft: 2,
+                textAlign: "center",
+                justifyContent: "center",
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "center",
+              }}
+            >
+              <Typography variant='h3'>{article.name}</Typography>
+              <Typography variant='h4'>{article.price}€</Typography>
+              <Typography sx={{ margin: "20px 0" }}>
+                {article.content}
+              </Typography>
+              <Grid container>
+                {article.size.map((s, index) => (
+                  <Grid
+                    item
+                    xs={6}
+                    sx={{ border: "1px solid lightgray", padding: 2 }}
+                    key={index}
+                  >
+                    {s}
+                  </Grid>
+                ))}
+              </Grid>
+            </Box>
+          </Grid>
+        </>
       )}
     </>
   );
